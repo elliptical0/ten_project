00b30e0faa570fad9afc4e0d0a1a2ad570ef3521
Elise Maino <mainot@wit.edu>
2023-03-16 16:22:52 -0400

    metroid crawls

---
+++ build/metr.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(metr)
+
+//======================================================================
+//
+//	metr, 64x64@4, 
+//	+ palette 256 entries, not compressed
+//	+ 64 tiles not compressed
+//	Total size: 512 + 2048 = 2560
+//
+//	Time-stamp: 2023-02-27, 16:45:41
+//	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_METR_H
+#define GRIT_METR_H
+
+#define metrTilesLen 2048
+extern const unsigned int metrTiles[512];
+
+#define metrPalLen 512
+extern const unsigned short metrPal[256];
+
+#endif // GRIT_METR_H
+
+//}}BLOCK(metr)
