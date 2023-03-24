a41b3e2d3ed0fa9d389dc0103f3d4ba8266bad58
Elise Maino <mainot@wit.edu>
2023-03-23 23:26:26 -0400

    a bunch of rendering stuff, including an environment background

--- source/main.c
+++ source/main.c
@@ -2,21 +2,12 @@
 
 #include <stdlib.h>
 
-#include <tonc_types.h>
-
-//#include "input.h"
-//#include "game.h"
+#include "input.h"
+#include "game.h"
 #include "render.h"
 
 int main(void)
 {
-	irq_init(NULL);
-	irq_enable(II_VBLANK);
-
-	tte_init_chr4c_default(0, BG_CBB(0) | BG_SBB(31));
-	tte_set_pos(92, 68);
-	tte_write(">-<");
-
 	// allocate the input and output structs for input, game, and render, and run init
 	INPUTSTATE* inputstate = malloc(sizeof(INPUTSTATE));
 	int inputerror = inputinit(inputstate); // TODO: possible error handling
--- source/render.c
+++ source/render.c
@@ -1,18 +1,43 @@
 #include "render.h"
 
+#include "tonc_memmap.h"
+
 OBJ_ATTR obj_buffer[128];
 OBJ_AFFINE *obj_aff_buffer = (OBJ_AFFINE*)obj_buffer;
+    
+/**
+ * loads the tileset into memory
+*/
+void loadTileset(enum tileset ts) {
+    switch(ts) {
+        case field:
+            memcpy32(&tile_mem[ENV_CB][0], tsfieldTiles, tsfieldTilesLen / 4);
+            memcpy32(&pal_bg_mem[0], tsfieldPal, tsfieldPalLen / 4);
+            break;
+    }
+}
+
+void loadMap(enum map m) {
+    switch(m) {
+        case test:
+            memcpy32(&se_mem[ENV_SB], map0, map0Length / 4);
+            loadTileset(field);
+            break;
+    }
+}
 
 int renderinit(RENDERSTATE* renderstate) {
     // copied from load_sprite
     memcpy32(&tile_mem[4][0], metrTiles, metrTilesLen / 4);
 	memcpy32(pal_obj_mem, metrPal, metrPalLen / 4);
+    //memcpy32(&tile_mem[4][0], testTiles, testTilesLen / 4);
+	//memcpy32(pal_obj_mem, testPal, testPalLen / 4);
 
 	oam_init(obj_buffer, 128);
 
 	renderstate->metroid = &obj_buffer[0];
 	obj_set_attr(renderstate->metroid,
-		ATTR0_SQUARE,  // Square, regular sprite
+		ATTR0_SQUARE || ATTR0_8BPP,  // Square, regular sprite, 8 bits per pixel
 		ATTR1_SIZE_64, // 64x64 pixels,
 		ATTR2_PALBANK(0) | 0); // palette index 0, tile index 0
 
@@ -21,8 +46,23 @@ int renderinit(RENDERSTATE* renderstate) {
 
 	oam_copy(oam_mem, obj_buffer, 1); // Update first OAM object
 
-    // copied from right after load_sprite
-    REG_DISPCNT = DCNT_MODE0 | DCNT_BG0 | DCNT_OBJ | DCNT_OBJ_1D;
+    // copied from main
+    irq_init(NULL);
+	irq_enable(II_VBLANK);
+
+    loadMap(test);
+
+    // set background control registers
+    REG_BGCNT[ENV_BG] = env_bgcnt;
+    REG_BGCNT[UI_BG] = ui_bgcnt;
+
+    // initialize text engine
+	tte_init_chr4c_default(TEXT_BG, text_bgcnt);
+	tte_set_pos(92, 68);
+	tte_write(">-<");
+
+    // set display control register
+    REG_DISPCNT = dcnt;
 
     return 0;
 }
